5988ee4804cec0f0c24d809f2f8966a0350c3e62
Matheus <matheus.wachtler@outlook.com>
2023-06-01 00:33:49 -0300

    update project with new classes. add classes InventoryPage

--- src/test/java/pages/InventoryPage.java
+++ src/test/java/pages/InventoryPage.java
@@ -2,4 +2,26 @@ package pages;
 
 public class InventoryPage extends BasePage {
 
+    private static final String urlInventoryPage = "https://www.saucedemo.com/inventory.html";
+    private static final String idShoppingCart = "shopping_cart_container";
+    private static final String idAddItemToCart = "add-to-cart-sauce-labs-backpack";
+    private static final String idRemoveItemToCart = "remove-sauce-labs-backpack";
+
+    public static String getUrlInventoryPage() {
+        return urlInventoryPage;
+    }
+
+    public static String getIdShoppingCart() {
+        return idShoppingCart;
+    }
+
+
+    public static String getIdAddItemToCart() {
+        return idAddItemToCart;
+    }
+
+    public static String getIdRemoveItemToCart() {
+        return idRemoveItemToCart;
+    }
+
 }
--- src/test/java/tests/InventoryTest.java
+++ src/test/java/tests/InventoryTest.java
@@ -1,4 +1,34 @@
 package tests;
 
-public class InventoryTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pages.InventoryPage;
+
+public class InventoryTest extends BaseTest {
+
+    InventoryPage inventoryPage = new InventoryPage();
+
+    @Test
+    public void isCurrentPage(){
+        inventoryPage.navigateToUrl(this.webDriver, InventoryPage.getUrlInventoryPage());
+        Assertions.assertTrue(inventoryPage.compareCurrentUrl(this.webDriver,InventoryPage.getUrlInventoryPage()));
+        Assertions.assertNotNull(inventoryPage.findElementById(this.webDriver,InventoryPage.getIdAddItemToCart()));
+        Assertions.assertNotNull(inventoryPage.findElementById(this.webDriver,InventoryPage.getIdShoppingCart()));
+    }
+
+    @Test
+    public void shouldAddItemToCart() {
+        inventoryPage.navigateToUrl(this.webDriver, InventoryPage.getUrlInventoryPage());
+        inventoryPage.findAndClickButtonById(this.webDriver,InventoryPage.getIdAddItemToCart());
+        Assertions.assertNotNull(inventoryPage.findElementById(this.webDriver,InventoryPage.getIdRemoveItemToCart()));
+    }
+
+    @Test
+    public void shouldRemoveItemToCart() {
+        shouldAddItemToCart();
+        inventoryPage.findAndClickButtonById(this.webDriver,InventoryPage.getIdRemoveItemToCart());
+        Assertions.assertNotNull(inventoryPage.findElementById(this.webDriver,InventoryPage.getIdAddItemToCart()));
+    }
+
 }
+
